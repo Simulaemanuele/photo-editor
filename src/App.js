e84d24b8094f91ab8553d32c7e8bed9eb7270d00
Emanuele Simula <simulaemanuele@gmail.com>
2023-01-19 18:47:01 +0100

    feat: sidebar functionality implemented & updated default options constant

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./App.css";
 import SidebarItem from "./SidebarItem";
 import Slider from "./Slider";
@@ -33,15 +34,66 @@ const DEFAULT_OPTIONS = [
     },
     unit: "%",
   },
+  {
+    name: "Grayscale",
+    property: "grayscale",
+    value: 0,
+    range: {
+      min: 0,
+      max: 100,
+    },
+    unit: "%",
+  },
+  {
+    name: "Sepia",
+    property: "sepia",
+    value: 0,
+    range: {
+      min: 0,
+      max: 100,
+    },
+    unit: "%",
+  },
+  {
+    name: "Hue Rotate",
+    property: "hue-rotate",
+    value: 0,
+    range: {
+      min: 0,
+      max: 360,
+    },
+    unit: "deg",
+  },
+  {
+    name: "Blur",
+    property: "blur",
+    value: 0,
+    range: {
+      min: 0,
+      max: 20,
+    },
+    unit: "px",
+  },
 ];
 
 function App() {
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0);
+  const [options, setOptions] = useState(DEFAULT_OPTIONS);
+
+  const selectedOption = options[selectedOptionIndex];
+
   return (
     <div className="container">
       <div className="main-image" />
       <div className="sidebar">
-        <SidebarItem />
-        <SidebarItem />
+        {options.map((option, index) => (
+          <SidebarItem
+            key={index}
+            name={option.name}
+            active={index === selectedOptionIndex}
+            handleClick={() => setSelectedOptionIndex(index)}
+          />
+        ))}
       </div>
       <Slider />
     </div>
