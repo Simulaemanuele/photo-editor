4923121bf91eab197000a9d71792bde76f997fbc
Emanuele Simula <simulaemanuele@gmail.com>
2023-01-19 17:30:46 +0100

    feat: sidebar item component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import SidebarItem from "./SidebarItem";
 import Slider from "./Slider";
 
 function App() {
   return (
     <div className="container">
       <div className="main-image" />
-      <div className="sidebar">Sidebar</div>
+      <div className="sidebar">
+        <SidebarItem />
+        <SidebarItem />
+      </div>
       <Slider />
     </div>
   );
